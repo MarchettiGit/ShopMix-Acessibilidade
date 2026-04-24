257973a0bb1566b88e2c9ba20e4aa1e8c6019639
MarchettiGit <murilomarchetti.3105@gmail.com>
2026-04-24 08:56:59 -0300

    Ajuste textual e conexão com a proxima página

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,15 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @PostMapping("/login")
+    public String fazerLogin() {
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "home";
     }
 }
